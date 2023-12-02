5786891e1a701ea682fd683f36350ed88bd68a6b
Jade Pennig <jade@pennig.name>
2023-12-02 13:53:39 -0600

    Team tabs

--- Tests/Day01.swift
+++ Tests/Day01.swift
@@ -12,23 +12,23 @@ import XCTest
 final class Day01Tests: XCTestCase {
 	func testPart1() throws {
 		let testData = """
-		one8sd2d2
-		1sixseven29
-		oo2dd
-		""" // 82 + 19 + 22 = 123
-
+			one8sd2d2
+			1sixseven29
+			oo2dd
+			""" // 82 + 19 + 22 = 123
+		
 		let challenge = Day01(data: testData)
 		XCTAssertEqual(String(describing: challenge.part1()), "123")
 	}
 	
 	func testPart2() throws {
 		let testData = """
-		1eightwo
-		eightwo1
-		two0
-		eightwo
-		7
-		""" // 12 + 81 + 20 + 82 + 77 = 272
+			1eightwo
+			eightwo1
+			two0
+			eightwo
+			7
+			""" // 12 + 81 + 20 + 82 + 77 = 272
 		
 		let challenge = Day01(data: testData)
 		XCTAssertEqual(String(describing: challenge.part2()), "272")
