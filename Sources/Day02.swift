d15999b4a4b83abef74a3e6818d83f5fad24c119
Jade Pennig <jade@pennig.name>
2023-12-02 13:59:16 -0600

    Add Day 2, Part 1 (first pass on data parsing; todo: look into RegexBuilder)

---
+++ Sources/Day02.swift
@@ -0,0 +1,106 @@
+//
+//  Day02.swift
+//
+//
+//  Created by Jade Pennig on 12/2/23.
+//
+
+import Foundation
+
+struct Day02: AdventDay {
+	// Save your data in a corresponding text file in the `Data` directory.
+	var data: String
+
+	// Constants for cube counts
+	let cubeCounts = [
+		"red": 12,
+		"green": 13,
+		"blue": 14
+	]
+	
+	/**
+	 * Parses data to provide structured data set for code challenge.
+	 */
+	var games: [[String: [[String: Int]]]] {
+		data.split(separator: "\n").map {
+			// 1. Parse the game ID
+			let gameIndexPattern = #/Game (\d+)/#
+			var gameIndex: String = ""
+			
+			if let gameIndexSearch = try! gameIndexPattern.firstMatch(in: String($0)) {
+				gameIndex = String(gameIndexSearch.1)
+			}
+			
+			// 2. Parse the data for each game
+			let gamesPattern = #/: (.*)/#
+			var gamesList: [Substring] = []
+			
+			if let gameDataSearch = try! gamesPattern.firstMatch(in: String($0)) {
+				gamesList = gameDataSearch.1.split(separator: ";").map {
+					return $0
+				}
+			}
+			
+			// 3. Parse the data for each pull of the dice
+			let pullCountPattern = #/(\d+) (\w+)/#
+			var gameData: [[String: Int]] = []
+
+			for game in gamesList {
+				let pulls = game.split(separator: ",")
+				var pullData: [String: Int] = [:]
+
+				for pull in pulls {
+					if let pullCountSearch = try! pullCountPattern.firstMatch(in: pull) {
+						pullData[String(pullCountSearch.2)] = Int(pullCountSearch.1)
+					}
+				}
+
+				gameData.append(pullData)
+			}
+			
+			return [
+				gameIndex: gameData
+			]
+		}
+	}
+		
+	
+	/**
+	 * Determines if the game result is a valid game based on the cube restraints
+	 * If it is a valid game, it returns the ID of the game. If it is not, it returns 0.
+	 */
+	private func isGamePossible(game: [[String: Int]]) -> Bool {
+		// valid until proven invalid
+		var isValidGame = true
+		
+		for grab in game {
+			for color in grab {
+				if (cubeCounts[color.key] ?? 0 < color.value) {
+					isValidGame = false
+				}
+			}
+		}
+		
+		return isValidGame
+	}
+	
+	
+	// Solving Part 1 of the daily challenge
+	func part1() -> Any {
+		var total: Int = 0
+		
+		for game in games {
+			if isGamePossible(game: game.values.first!) {
+				total += Int(game.keys.first ?? "0")!
+			}
+		}
+
+		return total
+	}
+
+	
+	// Solving Part 2 of the daily challenge
+	func part2() -> Any {
+		return 0
+	}
+}
